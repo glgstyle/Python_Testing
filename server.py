b28f85d9a86410126df52ca419f433496421e59b
glgstyle <gleguenno@gmail.com>
2022-12-30 10:37:31 +0100

    Bug fixed, all tests passed, loop function to search club by email added

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json, os
-from flask import Flask,render_template,request,redirect,flash,url_for,abort
+from flask import Flask,render_template,request,redirect,flash,url_for
 from config import Config, DevelopmentConfig
+
 
 def loadClubs():
     with open('clubs.json') as c:
@@ -29,17 +30,36 @@ def index():
 
 # @app.route('/showSummary',methods=['POST'])
 # def showSummary():
-#     club = [club for club in clubs if club['email'] == request.form['email']][0]
-#     return render_template('welcome.html',club=club,competitions=competitions)
+#     try:
+#         club = [club for club in clubs if club['email'] == request.form['email']][0]
+#         return render_template('welcome.html',club=club,competitions=competitions)
+#     except IndexError:
+#         message = "Sorry, that email wasn't found."
+#         flash(message)
+#         return render_template('index.html'), 401
 
+
+def search_club_by_email(clubs, email):
+    found_club = None
+    for club in clubs:
+        if club['email'] == email:
+            found_club = club
+            break
+    return found_club
 @app.route('/showSummary',methods=['POST'])
-def showSummary():
-    try:
-        club = [club for club in clubs if club['email'] == request.form['email']][0]
-        return render_template('welcome.html',club=club,competitions=competitions)
-    except IndexError:
-        abort(401, description="Sorry, that email wasn't found.")  
 
+
+def showSummary():
+    clubs = loadClubs()
+    club = search_club_by_email(clubs, email=request.form['email'])
+    if not club:
+        message = "Sorry, that email wasn't found."
+        flash(message)
+        return render_template('index.html'), 401
+    else:
+        return render_template('welcome.html',club=club,competitions=competitions)
+    
+ 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
     foundClub = [c for c in clubs if c['name'] == club][0]
@@ -61,9 +81,12 @@ def purchasePlaces():
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
-# TODO: Add route for points display
-
-
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+
+# TODO: 
+# Add route for points display
+
+    
