af4033857b7cd02059e6c41e3ac86e4d07351d2f
glgstyle <gleguenno@gmail.com>
2023-01-04 14:44:38 +0100

    Add a function to load competition in tests

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -1,5 +1,15 @@
 # They should not be able to redeem more points than available; this should be done within the UI. 
 # The redeemed points should be correctly deducted from the club's total.
+import json
+
+# ecrire une fonction qui ouvre les competitions et recupère les données de la compéitions si il la rtouve; 
+def loadCompetition(competition_name):
+    with open('competitions.json') as comps:
+        listOfCompetitions = json.load(comps)['competitions']
+        for comp in listOfCompetitions:
+          if comp['name'] == competition_name:
+            return comp
+        return None
 
 
 # book with sufficient points balance
@@ -20,13 +30,18 @@ def test_insufficient_points_balance_should_return_error(client):
     """
     Test club book a competition with insufficient points balance.
     """
+    competition = loadCompetition("Spring Festival")
+    required_places = 16
     response= client.post('/purchasePlaces', data={
       "club": "She Lifts",
-      "competition": "Spring Festival",
-      "places": 16
+      "competition": competition['name'],
+      "places": required_places
     })
     assert response.status_code == 405
-    # assert "You have " + response.data['points'] + " points which is not enough to book " + response.data['places'] + " places."  in response.data.decode('UTF-8')
+    text_search = "You have " + str(15) + " points which is not enough to book " + str(required_places) + " places."
+    print("text_search", text_search)
+    print("decode :", response.data.decode('UTF-8'))
+    assert text_search in response.data.decode('UTF-8')
 
 
 # if placesRequired > availablePlaces
@@ -62,10 +77,11 @@ def test_booked_places_should_subtract_club_points(client):
     Test deduction of club points when club books a competition
     with points of club = 15.  
     """
+    club_points = 15
     response= client.post('/purchasePlaces', data={
       "club": "She Lifts",
       "competition": "Spring Festival",
       "places": 3
     })
     assert response.status_code == 200
-    assert "Points available: 12" in response.data.decode('UTF-8')
+    assert "Points available: " + str(club_points - 3) in response.data.decode('UTF-8')
