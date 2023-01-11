c66376df69d496c320244804b223552359c9b961
glgstyle <gleguenno@gmail.com>
2023-01-11 15:02:38 +0100

    Changes about test coverage

--- tests/unit_tests/test_display.py
+++ tests/unit_tests/test_display.py
@@ -16,14 +16,27 @@ def test_should_display_list_of_clubs(client):
 
 def test_book_should_load_competition(client):
     """Test if the competition to book is well displayed on page."""
-    response = client.get('/book/<competition>/<club>')
     club = "She Lifts"
     foundClub = loadClub(club) 
     competition = "Freebie Shark" 
     foundCompetition = loadCompetition(competition) 
+    response = client.get('/book/'+ competition + '/' + club)
     name = foundCompetition['name']
     date = foundCompetition['date']
     places = foundCompetition['numberOfPlaces']
-    expecting_data =  "<li>\n            "+ name + "<br />\n            Date: " + date + "</br>\n            Number of Places: " + places + "\n            \n            "
+    expecting_data = "Places available: " + places
+    assert expecting_data in response.data.decode('UTF-8')
+    assert response.status_code == 200
+
+
+def test_book_should_failed_load_competition(client):
+    """Test if the competition to book is well displayed on page."""
+    club = "Unknow club"
+    foundClub = loadClub(club) 
+    competition = "Freebie Shark" 
+    response = client.get('/book/'+ competition + '/' + club)
+    content = response.data.decode('UTF-8')
+    expecting_data = "Something went wrong-please try again"
+    print(content)
     assert expecting_data in response.data.decode('UTF-8')
     assert response.status_code == 200
