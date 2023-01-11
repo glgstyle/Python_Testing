0505acbea0492d89bbcedfb78958b46e8c51c309
glgstyle <gleguenno@gmail.com>
2023-01-11 14:18:38 +0100

    Remove feature display board points to put in another branch

--- tests/test_display.py
+++ tests/test_display.py
@@ -2,18 +2,6 @@ from .utils import loadClub, loadClubs, loadCompetition
 # They should be able to see the list of clubs and their associated current points balance
 
 
-def test_should_display_list_of_clubs(client):
-    """Test if the list of clubs well displayed on page."""
-    response = client.get('/pointsDisplayBoard')
-    clubs = loadClubs()
-    for club in clubs:
-        name = club['name']
-        points = club['points']
-        expecting_data = "<li>\n            "+ name + "<br />\n            " + "Points: " + points + "</br>\n        </li>\n"
-        assert expecting_data in response.data.decode('UTF-8')
-    assert response.status_code == 200
-
-
 def test_book_should_load_competition(client):
     """Test if the competition to book is well displayed on page."""
     response = client.get('/book/<competition>/<club>')
